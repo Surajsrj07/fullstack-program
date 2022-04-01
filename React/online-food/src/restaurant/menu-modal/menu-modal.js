a959f8e42461c6705fda3d5aca87d06a1d009069
Surajsrj07 <Surajsrj93@gmail.com>
2022-04-01 13:05:46 +0530

    adding txt

--- React/online-food/src/restaurant/menu-modal/menu-modal.js
+++ React/online-food/src/restaurant/menu-modal/menu-modal.js
@@ -1,27 +1,38 @@
-import {Modal, Button} from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 
-function MenuModal(props){
-    return(
-        //<Modal show={show} onHide={handleClose}>
-        <Modal show={props.show}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-         { /*<Button variant="secondary" onClick={handleClose}>
+function MenuModal(props) {
+  return (
+    //<Modal show={show} onHide={handleClose}>
+    <Modal show={props.show}>
+      <Modal.Header closeButton>
+        <Modal.Title>Sea food Resto</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="card w-100">
+          <h5>Gobi Manjurian</h5>
+          <h6>`150</h6>
+        </div>
+        <div className="card w-100">
+            <h5>Panner Tikka</h5>
+            <h6>`140</h6>
+
+        </div>
+
+      </Modal.Body>
+      <Modal.Footer>
+        { /*<Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
             Save Changes
     </Button>*/}
-        </Modal.Footer>
-      </Modal>
+      </Modal.Footer>
+    </Modal>
 
 
 
-    )
+  )
 }
 
 export default MenuModal;
