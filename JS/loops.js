ac9787b98f603f9c07e7e312b75270f2d42597a5
Surajsrj07 <Surajsrj93@gmail.com>
2023-01-30 19:08:06 +0530

    change in var

--- JS/loops.js
+++ JS/loops.js
@@ -1,7 +1,7 @@
 function printOddTill100(){
 
     var i=1;                     //Assignment
-    while(i<=100){               //Evaluation
+    while(i<=200){               //Evaluation
         if(i%2!=0){
             console.log(i);
         }
@@ -13,7 +13,7 @@ printOddTill100();
 
 function printEvenTill100(){
 
-    for(let i=1 ; i<=100 ; i++){
+    for(let i=1 ; i<=200 ; i++){
         if(i%2==0){
             console.log(i);
         }
